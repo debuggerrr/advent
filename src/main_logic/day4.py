5efa9be71eb3d72219c72e7865a1d271c662548c
sid <siddhesh.kalgaonkar@agilelab.it>
2022-12-04 13:39:53 +0530

    updated day 4 solution for linear time complexity and constant space

--- src/main_logic/day4.py
+++ src/main_logic/day4.py
@@ -1,51 +1,31 @@
-import re
-
-
 class Day4(object):
 
     def part1(self, data):
-        pattern = r"(\d+)\-(\d+)"
         counter = 0
         for lines_data in data.splitlines():
-            first_range = ""
-            second_range = ""
             lines_data = lines_data.strip()
             first_elf, second_elf = lines_data.split(",")[0], lines_data.split(",")[1]
-            first_match, second_match = re.findall(pattern, first_elf), re.findall(pattern, second_elf)
-            for tuple in first_match:
-                low = int(tuple[0])
-                high = int(tuple[1])
-                first_range = list(range(low, high + 1))
-            for tuple in second_match:
-                low = int(tuple[0])
-                high = int(tuple[1])
-                second_range = list(range(low, high + 1))
-            check_first_side = all(item in first_range for item in second_range)
-            check_second_side = all(item in second_range for item in first_range)
-            if check_first_side or check_second_side:
+            first_range_start, first_range_end = map(int, first_elf.split("-"))
+            second_range_start, second_range_end = map(int, second_elf.split("-"))
+            first_range = list(range(first_range_start, first_range_end + 1))
+            second_range = list(range(second_range_start, second_range_end + 1))
+            check_first_side_if_returns_true = all(item in first_range for item in second_range)
+            check_second_side_if_returns_true = all(item in second_range for item in first_range)
+            if check_first_side_if_returns_true or check_second_side_if_returns_true:
                 counter += 1
         return counter
 
-
     def part2(self, data):
-        pattern = r"(\d+)\-(\d+)"
         counter = 0
         for lines_data in data.splitlines():
-            first_range = ""
-            second_range = ""
             lines_data = lines_data.strip()
             first_elf, second_elf = lines_data.split(",")[0], lines_data.split(",")[1]
-            first_match, second_match = re.findall(pattern, first_elf), re.findall(pattern, second_elf)
-            for tuple in first_match:
-                low = int(tuple[0])
-                high = int(tuple[1])
-                first_range = list(range(low, high + 1))
-            for tuple in second_match:
-                low = int(tuple[0])
-                high = int(tuple[1])
-                second_range = list(range(low, high + 1))
-            check_first_side = any(item in first_range for item in second_range)
-            check_second_side = any(item in second_range for item in first_range)
-            if check_first_side or check_second_side:
+            first_range_start, first_range_end = map(int, first_elf.split("-"))
+            second_range_start, second_range_end = map(int, second_elf.split("-"))
+            first_range = list(range(first_range_start, first_range_end + 1))
+            second_range = list(range(second_range_start, second_range_end + 1))
+            check_first_side_if_returns_true = any(item in first_range for item in second_range)
+            check_second_side_if_returns_true = any(item in second_range for item in first_range)
+            if check_first_side_if_returns_true or check_second_side_if_returns_true:
                 counter += 1
         return counter
