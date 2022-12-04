28f75a0c3daeecb6cf2b302bf45b97aad03d127d
sid <siddhesh.kalgaonkar@agilelab.it>
2022-12-04 22:33:26 +0530

    Updated the time complexity for day4

--- src/main_logic/day4.py
+++ src/main_logic/day4.py
@@ -4,21 +4,22 @@ class Day4(object):
         counter = 0
         for lines_data in data.splitlines():
             lines_data = lines_data.strip()
-            first_range, second_range = self.__get_first_second_list_of_elements(lines_data)
-            check_first_side_if_returns_true = all(item in first_range for item in second_range)
-            check_second_side_if_returns_true = all(item in second_range for item in first_range)
-            if check_first_side_if_returns_true or check_second_side_if_returns_true:
+            first_range_start, first_range_end, second_range_start, second_range_end = \
+                self.__get_first_second_list_of_elements(lines_data)
+            if (first_range_start >= second_range_start and first_range_end <= second_range_end) or \
+                    (second_range_start >= first_range_start and second_range_end <= first_range_end):
                 counter += 1
         return counter
 
-    def part2(self, data):
+    def part2(self, data):  # Still a brute force time complexity solution
         counter = 0
         for lines_data in data.splitlines():
             lines_data = lines_data.strip()
-            first_range, second_range = self.__get_first_second_list_of_elements(lines_data)
-            check_first_side_if_returns_true = any(item in first_range for item in second_range)
-            check_second_side_if_returns_true = any(item in second_range for item in first_range)
-            if check_first_side_if_returns_true or check_second_side_if_returns_true:
+            first_range_start, first_range_end, second_range_start, second_range_end = \
+                self.__get_first_second_list_of_elements(lines_data)
+            first_range = list(range(first_range_start, first_range_end + 1))
+            second_range = list(range(second_range_start, second_range_end + 1))
+            if set(first_range).intersection(second_range):
                 counter += 1
         return counter
 
@@ -26,6 +27,4 @@ class Day4(object):
         first_elf, second_elf = data.split(",")[0], data.split(",")[1]
         first_range_start, first_range_end = map(int, first_elf.split("-"))
         second_range_start, second_range_end = map(int, second_elf.split("-"))
-        first_range = list(range(first_range_start, first_range_end + 1))
-        second_range = list(range(second_range_start, second_range_end + 1))
-        return first_range, second_range
+        return first_range_start, first_range_end, second_range_start, second_range_end
