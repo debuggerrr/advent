9737965f9d6353563ab5722df5b2516917999722
sid <siddhesh.kalgaonkar@agilelab.it>
2022-12-04 13:42:59 +0530

    updated day 4 solution for linear time complexity and constant space

--- src/main_logic/day4.py
+++ src/main_logic/day4.py
@@ -4,11 +4,7 @@ class Day4(object):
         counter = 0
         for lines_data in data.splitlines():
             lines_data = lines_data.strip()
-            first_elf, second_elf = lines_data.split(",")[0], lines_data.split(",")[1]
-            first_range_start, first_range_end = map(int, first_elf.split("-"))
-            second_range_start, second_range_end = map(int, second_elf.split("-"))
-            first_range = list(range(first_range_start, first_range_end + 1))
-            second_range = list(range(second_range_start, second_range_end + 1))
+            first_range, second_range = self.__get_first_second_list_of_elements(lines_data)
             check_first_side_if_returns_true = all(item in first_range for item in second_range)
             check_second_side_if_returns_true = all(item in second_range for item in first_range)
             if check_first_side_if_returns_true or check_second_side_if_returns_true:
@@ -19,13 +15,17 @@ class Day4(object):
         counter = 0
         for lines_data in data.splitlines():
             lines_data = lines_data.strip()
-            first_elf, second_elf = lines_data.split(",")[0], lines_data.split(",")[1]
-            first_range_start, first_range_end = map(int, first_elf.split("-"))
-            second_range_start, second_range_end = map(int, second_elf.split("-"))
-            first_range = list(range(first_range_start, first_range_end + 1))
-            second_range = list(range(second_range_start, second_range_end + 1))
+            first_range, second_range = self.__get_first_second_list_of_elements(lines_data)
             check_first_side_if_returns_true = any(item in first_range for item in second_range)
             check_second_side_if_returns_true = any(item in second_range for item in first_range)
             if check_first_side_if_returns_true or check_second_side_if_returns_true:
                 counter += 1
         return counter
+
+    def __get_first_second_list_of_elements(self, data):
+        first_elf, second_elf = data.split(",")[0], data.split(",")[1]
+        first_range_start, first_range_end = map(int, first_elf.split("-"))
+        second_range_start, second_range_end = map(int, second_elf.split("-"))
+        first_range = list(range(first_range_start, first_range_end + 1))
+        second_range = list(range(second_range_start, second_range_end + 1))
+        return first_range, second_range
